872620386f05e30a2e9a35f1f09df3338a74fd9f
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-04-07 15:31:34 -0400

    add todo

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,6 +2,17 @@ import { Text } from 'components';
 import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
 import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ value, item }) => {
+  console.log(item);
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #{item}
+      </Text>
+      <Text>{value}</Text>
+      <DeleteButton type="button">
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,34 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+
+  addTodo = evt => {
+    const todo = {
+      id: nanoid(),
+      text: evt,
+    };
+    this.setState(({ todos }) => ({
+      todos: [...todos, todo],
+    }));
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    return (
+      <>
+        <SearchForm onSubmit={this.addTodo} />
+        <Grid>
+          {this.state.todos.map(({ text, id }, idx) => {
+            return (
+              <GridItem key={id}>
+                <Todo value={text} item={idx + 1} />
+              </GridItem>
+            );
+          })}
+        </Grid>
+      </>
+    );
   }
 }
